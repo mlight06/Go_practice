2719ee6b3994a7cf070ecf7590bf234742cb0219
Michael Light <mklight06@gmail.com>
2024-03-19 13:43:13 -0600

    added handler functions

--- server.go
+++ server.go
@@ -11,3 +11,13 @@ import (
 func main() {
 	fmt.Printf("Starting server at port 8080\n")
 }
+
+func serveRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "this is my website!]n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request \n")
+	io.WriteString(w, "Hello, Http!\n")
+}
