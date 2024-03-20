ef3a38744bae7fb47b06cc9450cf9c4e81133ed4
Michael Light <mklight06@gmail.com>
2024-03-20 11:24:50 -0600

    functioning server with 2 endpoints

--- server.go
+++ server.go
@@ -13,11 +13,18 @@ func main() {
 	http.HandleFunc("/hello", getHello)
 
 	err := http.ListenAndServe(":3333", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+} else if err != nil {
+	fmt.Printf("error starting server: %s\n", err)
+	os.Exit(1)
+	}
 }
 
 func serveRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
-	io.WriteString(w, "this is my website!]n")
+	io.WriteString(w, "this is my website!\n")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
