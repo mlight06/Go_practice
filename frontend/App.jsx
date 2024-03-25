30b4772649cb380eec54ca6123b93cde6e6d75b5
Michael Light <mklight06@gmail.com>
2024-03-25 11:53:45 -0600

    adding react dom, building frontend

---
+++ frontend/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <h1>
+          REACT APP
+        </h1>
+      </div>
+    </div>
+  )
+}
